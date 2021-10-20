ff4e032542837083d640ceda6b63518695067675
Luiz Henrique <7henrique18@gmail.com>
2021-10-20 02:01:23 -0300

    feat: route get last 3 messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { GetLast3MessagesService } from "./services/GetLast3MessagesService";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+router.get("/messages/last3/", new GetLast3MessagesController().handle);
 
 export { router };
